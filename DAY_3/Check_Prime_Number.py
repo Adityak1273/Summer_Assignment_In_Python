aba8cd484c1232f5f665b41d5865370dc12f748b
Aditya_Kumar <adityak1273@gmail.com>
2026-06-01 20:55:23 +0530

    WEEK_1

--- DAY_3/Check_Prime_Number.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the Number : "))
-notprime=0
-for i in range(2,n):
-    if(n%i==0):
-        notprime+=1
-if(n==1 or n==0 or notprime==1):
-    print(n," is not prime number.")
-else:
-    print(n," is prime number.")
